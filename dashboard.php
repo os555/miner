d6a96b971f954f940bfed002222b79fd140d73a8
wristyquill <adonis.darwish@gmail.com>
2017-12-12 19:08:57 +0400

    API improvements

--- dashboard.php
+++ dashboard.php
@@ -15,7 +15,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    <script type="text/javascript" src="dist/js/miner.js"></script>
+
     <title>Miner Monitoring Dashboard</title>
 <header>
 <div class="wrapper navbar-fixed">
@@ -36,39 +36,17 @@
     </div></li>
     <li class="active"><a href="#!" onclick="loadmain();"><i class="material-icons">dashboard</i>Main</a></li>
     <li><a href="#!" onclick="loadmonit();"><i class="material-icons">ac_unit</i>Temperatures</a></li>
-    <li><a href="#!" onclick="loadmonitold();"><i class="material-icons">remove_red_eye</i>Old Dashboard</a></li>
     <li><a href="#!" onclick="loadconfig();"><i class="material-icons">settings</i>Configuration</a></li>  
   </ul>
 <!-- END SIDE NAVIGATION BAR !-->
 </header>
 </head>
 <body>
-<!--
-<div class="row col s12">
-    <div class="input-field col s6">
-      <i class="material-icons prefix">ac_unit</i>
-      <input id="icon_prefix" type="text" class="validate">
-      <label for="icon_prefix">Temperature to warn (Default: 80)</label>
-    </div>
-    <div class="input-field col s6">
-      <i class="material-icons prefix">explicit</i>
-      <input id="maxhasheth" type="text" class="validate">
-      <label for="maxhasheth">Maximum Hashrate ETH (Default: 200)</label>
-    </div>
-    <div class="col s6 input-field">
-    <button class="btn waves-effect waves-light" type="submit" name="action" onclick="gettemp();">Submit
-        <i class="material-icons right">send</i>
-    </button>
-    </div>
-</div>
--->
 <div class="custcontainer">
 <!-- Page Content goes here -->
 </div>
 </body>
 <script>
-var deftemp = 80;
-var maxhasheth = 200;
 $(".button-collapse").sideNav({
       closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
       draggable: true // Choose whether you can drag to open on touch screens
@@ -78,15 +56,5 @@ function loadmonit(){$(".custcontainer").load("pages/tempmon.html");}
 function loadmonitold(){$(".custcontainer").load("pages/miner.php");}
 function loadconfig(){$(".custcontainer").load("pages/config.html");}
 $(document).ready(loadmain());
-
-function gettemp(){
-    if (parseInt($("#icon_prefix").val) != 0 ) {
-   deftemp = $("#icon_prefix").val(); } 
-    if (parseInt($("#maxhasheth").val) != 0 ) {
-    maxhasheth = parseInt($("#maxhasheth").val()); 
-    for (i=0; i < g.length; i++){
-        g[i].refresh(g[i].originalValue, maxhasheth);
-        }
-    }
-};
 </script>
+<script type="text/javascript" src="dist/js/miner.js"></script>
--- dist/js/miner.js
+++ dist/js/miner.js
@@ -1,8 +1,20 @@
 var currenthost = window.location.hostname;
+var deftemp = 80;
+var maxhasheth = 200;
 var gpucount = 0;
 var minegputemp = [];
 var socketStatus = document.getElementById('status');
-var socket = new WebSocket("ws://"+currenthost+":1880/ws/miner");
+var gaugediv = [];
+var gaugedivdecred = [];
+var g = [];
+var gdecred = [];
+var fanspattern = /^fan[0-9]+/;
+var tempspattern = /^temperature[0-9]+/;
+var ethratespattern = /^gpuethrate[0-9]+/;
+var dcrratespattern = /^gpudcrrate[0-9]+/;
+var ethrates =[];
+var dcrrates = [];
+var socket = new WebSocket("ws://"+currenthost+":1880/ws/socket");
 function miners() {
 	socket.onopen = function(event) {
 	  console.log('Connected to: ' + event.currentTarget.url);
@@ -11,4 +23,38 @@ function miners() {
 	  console.log('WebSocket Error: ' + error);
 	};
 }
+
+function gettemp(){
+    if (parseInt($("#icon_prefix").val) != 0 ) {
+   deftemp = $("#icon_prefix").val(); } 
+    if (parseInt($("#maxhasheth").val) != 0 ) {
+    maxhasheth = parseInt($("#maxhasheth").val()); 
+    for (i=0; i < g.length; i++){
+        g[i].refresh(g[i].originalValue, maxhasheth);
+        }
+    }
+    if (parseInt($("#maxhashdecred").val) != 0 ) {
+    maxhashdecred = parseInt($("#maxhashdecred").val()); 
+    for (i=0; i < g.length; i++){
+        gdecred[i].refresh(g[i].originalValue, maxhasheth);
+        }
+    }
+};
+
+function gage (evt, eth, dcr){
+  g[evt] = new JustGage({
+    id: gaugediv[evt],
+    value: eth,
+    min: 0,
+    max: maxhasheth,
+    title: "Total ETH rate"
+    });
+  gdecred[evt] = new JustGage({
+    id: gaugedivdecred[evt],
+    value: dcr,
+    min: 0,
+    max: 5000,
+    title: "Total Decred rate"
+    });
+};
 miners();
